e63ae580e4e7775f408f7a5b8a08e049cf088447
jmayeur <jmayeur@gmail.com>
2024-12-07 09:57:46 -0800

    Day 7

---
+++ 07/part1/runner.js
@@ -0,0 +1,74 @@
+const INPUT = require("../data");
+
+const parseData = (INPUT) => {
+  return INPUT.split("\n").map((row) => {
+    const [rawTotal, rawFactors] = row.split(": ");
+
+    return {
+      total: parseInt(rawTotal),
+      factors: rawFactors.split(" ").map(Number),
+    };
+  });
+};
+
+const getOperatorPermutations = (operators, operations) => {
+  if (getOperatorPermutations.cache[operations]) {
+    return getOperatorPermutations.cache[operations];
+  }
+  const result = [];
+
+  if (operations === 0) {
+    return [[]];
+  }
+
+  const subSet = getOperatorPermutations(operators, operations - 1);
+
+  for (let character of operators) {
+    for (let sub of subSet) {
+      result.push([character].concat(sub));
+    }
+  }
+
+  getOperatorPermutations.cache[operations] = result;
+  return result;
+};
+getOperatorPermutations.cache = {};
+
+const isEquationPossible = (total, factors) => {
+  const operatorPermutations = getOperatorPermutations(
+    ["+", "*"],
+    factors.length - 1
+  );
+
+  return operatorPermutations.reduce((result, operatorPermutation) => {
+    if (result > 0) {
+      return result;
+    }
+    const possibleTotal = factors.reduce((acc, factor, index) => {
+      if (index > 0) {
+        const operator = operatorPermutation[index - 1];
+        if (operator === "+") {
+          return acc + factor;
+        } else if (operator === "*") {
+          if (acc === 0) {
+            return 1 * factor;
+          }
+          return acc * factor;
+        }
+      }
+      return acc;
+    });
+    
+    if (possibleTotal === total) {
+        return possibleTotal;
+    }
+    return 0;
+  }, 0);
+
+
+};
+
+const start = performance.now();
+const data = parseData(INPUT);
+console.log(data.map(({ total, factors }) => isEquationPossible(total, factors)).reduce((a, v) => a + v));
+console.log(`Runtime ${performance.now() - start}ms`);
